f1c52b516ffeb6d8d9eac5fd2ae114836714b82e
hypercubist <qpinguz@gmail.com>
2022-03-27 21:01:49 +0900

    POPIN FIRST COMMIT

---
+++ src/main/java/io/summer/popin/PopinApplication.java
@@ -0,0 +1,13 @@
+package io.summer.popin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/summer/popin/PopinApplicationTests.java
@@ -0,0 +1,13 @@
+package io.summer.popin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
