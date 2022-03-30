53bf2dc9ea300816ad9c39d0fbdaa218d9a7d4c0
조형준 <choadd@naver.com>
2022-03-30 12:35:51 +0900

    3_30 mapping change

--- src/main/java/io/summer/popin/domain/statistics/controller/StatisticsApiController.java
+++ src/main/java/io/summer/popin/domain/statistics/controller/StatisticsApiController.java
@@ -3,8 +3,9 @@ package io.summer.popin.domain.statistics.controller;
 import io.summer.popin.domain.statistics.service.StatisticsService;
 import io.summer.popin.domain.statistics.vo.StatisticsVO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -13,12 +14,9 @@ public class StatisticsApiController {
 
     private final StatisticsService statisticsService;
 
-
-    @ResponseBody
-    @RequestMapping("/chart")
-    public StatisticsVO getChartData(int hostNo){
-
-        return statisticsService.getChartData(1);
+    @PostMapping("/chart")
+    public ResponseEntity<StatisticsVO> getChartData(Integer hostNo){
+        return new ResponseEntity<StatisticsVO>(statisticsService.getChartData(1), HttpStatus.OK) ;
     }
 
 
