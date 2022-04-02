a2216105dcda3f552d1813fa9836e4a5abc5f2ef
조형준 <choadd@naver.com>
2022-04-02 14:07:44 +0900

    4_02 배치 프로그램 test완료

--- src/main/java/io/summer/popin/domain/statistics/dao/StatisticsMapper.java
+++ src/main/java/io/summer/popin/domain/statistics/dao/StatisticsMapper.java
@@ -15,8 +15,8 @@ public interface StatisticsMapper {
     public List<StatisticsHostVO> statisticsChart(Integer hostNo);
 
     //호스트가 있는지 조회
-    public List<StatisticsHostVO> statisticsHostList();
+    public List<String> statisticsHostList();
 
     //스케줄러 돌릴 호스트별로 통계 테이블에 저장한다
-    public boolean statisticsScheduler();
+    public boolean statisticsScheduler(String hostNo);
 }
--- src/test/java/io/summer/popin/persistence/StatisticsMapperTest.java
+++ src/test/java/io/summer/popin/persistence/StatisticsMapperTest.java
@@ -1,7 +1,7 @@
 package io.summer.popin.persistence;
 
+import io.summer.popin.domain.statistics.dao.StatisticsMapper;
 import io.summer.popin.domain.statistics.service.SchedulerService;
-import io.summer.popin.domain.statistics.vo.StatisticsHostVO;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,12 +17,26 @@ public class StatisticsMapperTest {
     @Autowired
     SchedulerService schedulerService;
 
+    @Autowired
+    StatisticsMapper statisticsMapper;
+
     @DisplayName("호스트 번호 조회")
     @Test
     public void chartTest(){
-        List<StatisticsHostVO> list = schedulerService.statisticsHostList();
-            for (StatisticsHostVO i : list){
-                log.info(i.getHostNo().toString());
+//        List<String> list = schedulerService.statisticsHostList();
+//            for (String i : list){
+//                log.info(i.getHostNo().toString());
+//        }
+    }
+
+    @DisplayName("배치 프로그램")
+    @Test
+    public void insertTest(){
+        List<String> list =  statisticsMapper.statisticsHostList();
+
+        for (String i: list){
+            log.info(i);
+            statisticsMapper.statisticsScheduler(i);
         }
     }
 
