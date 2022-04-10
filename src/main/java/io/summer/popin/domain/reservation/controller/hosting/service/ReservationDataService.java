d4719bac455a978c0dea6cad99446d907eb3cf92
hypercubist <58693701+hypercubist@users.noreply.github.com>
2022-04-10 16:27:47 +0900

    delete hosting domain

--- src/main/java/io/summer/popin/domain/reservation/controller/hosting/service/ReservationDataService.java
+++
@@ -1,8 +0,0 @@
-package io.summer.popin.domain.reservation.controller.hosting.service;
-
-import io.summer.popin.domain.reservation.controller.hosting.dto.ReservationCountResponseDTO;
-
-public interface ReservationDataService {
-
-    public ReservationCountResponseDTO getReservationCount(Long memberNo);
-}
