8efe7c5d2985e2efbb2895c9376ce6b405718275
조형준 <choadd@naver.com>
2022-04-12 01:57:50 +0900

    search test clear!

--- src/main/java/io/summer/popin/domain/search/controller/SearchController.java
+++ src/main/java/io/summer/popin/domain/search/controller/SearchController.java
@@ -1,12 +1,12 @@
 package io.summer.popin.domain.search.controller;
 
 import io.summer.popin.domain.search.dto.SearchDTO;
+import io.summer.popin.domain.search.dto.SearchResponseDTO;
 import io.summer.popin.domain.search.service.SearchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -21,7 +21,13 @@ public class SearchController {
     public String searchList(@ModelAttribute SearchDTO searchDTO, Model model){
         log.info("SEARCH-TERM = {}", searchDTO);
         model.addAttribute("searchList",searchService.placesSearch(searchDTO));
-        log.info(String.valueOf(searchService.placesSearch(searchDTO)));
+        for(SearchResponseDTO i : searchService.placesSearch(searchDTO)){
+            log.info(i.toString());
+        }
+        log.info("test = {}",searchService.placesSearch(searchDTO).size());
         return "searchTest";
     }
+
+
+
 }
