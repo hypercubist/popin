252b16a15dd5efb6c0ad01714f3999f00708a0a7
조형준 <choadd@naver.com>
2022-04-23 16:00:58 +0900

    4_23 message ui

--- src/main/java/io/summer/popin/domain/message/controller/MessageController.java
+++ src/main/java/io/summer/popin/domain/message/controller/MessageController.java
@@ -1,14 +1,18 @@
 package io.summer.popin.domain.message.controller;
 
+import io.summer.popin.domain.member.dto.SessionUserDTO;
 import io.summer.popin.domain.message.service.MessageService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpSession;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/message")
@@ -18,9 +22,9 @@ public class MessageController {
 
     //메세지화면 view
     @GetMapping
-    public String messageView(Model model, HttpSession session){
-        session.setAttribute("senderNo",1);
-        model.addAttribute("messageDTO", messageService.myMessageList((Integer) session.getAttribute("senderNo")));
+    public String messageView(Model model, HttpSession session, @SessionAttribute("loginMember") SessionUserDTO loginMember){
+        log.info("SESSION-DATA ={}",loginMember.getNo());
+        model.addAttribute("messageDTO", messageService.myMessageList(loginMember.getNo()));
         return "html/message";
     }
 
